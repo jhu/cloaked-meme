20805ab9d325670419150c16ac74ade8430ee3e0
jhu <jhu5@gmu.edu>
2014-02-20 21:57:14 -0500

    adding core follow/unfollow functionality

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -112,6 +112,18 @@ describe "Authentication" do
         end
       end
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
